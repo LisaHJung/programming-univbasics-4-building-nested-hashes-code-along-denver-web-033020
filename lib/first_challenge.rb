3023139678e92816c62d4b355c0e7aa4fad79ae9
Lisa Jung <lisahjung@gmail.com>
2020-03-23 17:46:08 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,28 +1,7 @@
-epic_tragedy = {
-  :montague => {},
-  :capulet => {}
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
 def first_challenge
 epic_tragedy = {
   :montague => {},
   :capulet => {}
 }
 end
-=end
